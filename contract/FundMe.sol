bbbae69d1d154bfe32e70ea3efeefc7c88806dcb
oliseh6123 <oliseh.obiajuru@gmail.com>
2024-09-10 11:22:38 +0100

    Reverting fund

--- contract/FundMe.sol
+++ contract/FundMe.sol
@@ -6,11 +6,19 @@
 pragma solidity >= 0.8.2 < 0.9.2;
 
 contract FundMe {
+
+    uint256 public myValue = 1;
+    
     function fund() public payable {
         // Allow to send money $
         // Have a minimum $ sent
         // 1. How do we send ETH to this contract?
+        myValue = myValue + 2;
         require(msg.value > 1e18, "didn't send enough ETH"); // 1e18 = 1 ETH = 10000000000000000000 = 1 * 10 ** 18
+
+        // What is a revert?
+        // Undo any action that have been done, and send the remaining gas back
+
 
     }
     function withdraw() public{
