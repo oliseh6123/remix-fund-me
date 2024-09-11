aca7720432042025b9c66a1af6965811b84e84d6
oliseh6123 <oliseh.obiajuru@gmail.com>
2024-09-11 11:34:03 +0100

    getting USD to ETH conversionRate

--- contract/FundMe.sol
+++ contract/FundMe.sol
@@ -9,7 +9,7 @@ import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interf
 
 contract FundMe {
 
-    uint256 public minimumUsd = 5;
+    uint256 public minimumUsd = 5e18; //5 * 1e18
     
     function fund() public payable {
         // Allow to send money $
@@ -22,7 +22,7 @@ contract FundMe {
 
 
     }
-    function getPrice() view public returns (uint256){
+    function getPrice() public view returns (uint256){
         // Address 0x694AA1769357215DE4FAC081bf1f309aDC325306
         AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
         ( , int256 price,,,) = priceFeed.latestRoundData();
@@ -31,7 +31,14 @@ contract FundMe {
         // ABI
 
     }
-    function getConversionRate() public{
+    function getConversionRate(uint256 ethAmount) public view returns(uint256) {
+        // 1ETH?
+        //2000_000000000000000000
+        uint256 ethPrice = getPrice();
+        //(2000_000000000000000000 * 1_000000000000000000)/1e18
+        // $2000 = 1eth
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUsd;
 
     }
     function getVersion() view public returns(uint256) {
